b9176a62396a869d20a9d16802d0402669b25aa2
PatricNox <eronoxsmail2@gmail.com>
2018-11-24 23:00:08 +0100

    Get all PCs from Network

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+* Network-Admin
+* Main file for view output and command handling.
+*
+* @author PatricNox <hello@PatricNox.info>
+*
+*/
+
+require(__DIR__.'/nw/net.php');
---
+++ nw/net.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+* Network-Admin
+* This file involves all networking.
+*
+* @author PatricNox <hello@PatricNox.info>
+*
+*/
+
+/** WINDOWS Commands Executed
+ * 
+ *  arp 
+ *  nslookup
+ */
+
+
+/** Get IP from All Computers in LAN
+**************************************/
+// Query shell for Computer IP's in network
+$exec = explode('Type', shell_exec('arp -a'.' 2>&1'));
+
+// Only use IP list
+$ipList = explode('dynamic',$exec[1]);
+
+// Remove Static IP's
+end($ipList);
+$static = key($ipList);
+unset($ipList[$static]);
+reset($ipList);
+
+// Filter to only get IP
+// and save them in its own array
+$_IPList = [];
+for ($i = 1; $i < count($ipList); $i++)
+{
+    $d = explode(' ', $ipList[$i]);
+   
+    if ($ip = $d[5])
+        array_push($lanPC_IPList, $ip);
+}
+
+/** Get Hostname from All Computers in LAN
+**************************************/
+$_NameList = [];
+foreach ($lanPC_IPList as $PC)
+{
+    // Get and filter out hostnames
+    $exec = shell_exec("nslookup $PC".' 2>&1');
+    $exec = explode("Name:",$exec);
+    $name = explode("Address", $exec[1]);
+
+    // Remove lan tag and push into PCNames array
+    $PCName = str_replace('.lan', '', $name[0]);
+
+    array_push($lanPC_NameList, trim($PCName));
+}
